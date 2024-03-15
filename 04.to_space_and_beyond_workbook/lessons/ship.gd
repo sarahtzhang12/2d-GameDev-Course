6323baf8233e3ec09fbf67b4307f6ad35e498586
sarahtzhang12 <159182521+sarahtzhang12@users.noreply.github.com>
2024-03-15 12:16:34 -0700

    L5

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -3,7 +3,9 @@ extends Sprite2D
 var max_speed := (600.0)
 var velocity :=  Vector2 (0,0)
 var boost_speed := 1500.0
-var normal_speed := 600.0
+var normal_speed := 600.0 
+var steering_factor := 10.0
+
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
@@ -17,6 +19,9 @@ func _process(delta: float) -> void:
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 	
+	var desired_velocity := max_speed * direction  
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	velocity = direction * max_speed
 	position += velocity * delta 
 	
