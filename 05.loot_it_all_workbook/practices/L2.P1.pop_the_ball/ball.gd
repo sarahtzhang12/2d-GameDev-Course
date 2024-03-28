0927c0ee878fb955138340794e3ca2b0ea4ef862
sarahtzhang12 <159182521+sarahtzhang12@users.noreply.github.com>
2024-03-28 11:58:30 -0700

    L1 & L2 + Practices

--- 05.loot_it_all_workbook/practices/L2.P1.pop_the_ball/ball.gd
+++ 05.loot_it_all_workbook/practices/L2.P1.pop_the_ball/ball.gd
@@ -1,5 +1,13 @@
 extends Area2D
 
-
 func _ready() -> void:
-	pass
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area):
+	queue_free()	
+
+func _on_ball_2_area_entered(area):
+	queue_free()
+
+func _on_ball_3_area_entered(area):
+	queue_free()
